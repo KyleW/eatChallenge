47138c8adb3b35999a7804834c03f82687b2df9c
KyleW <Kyle.Warneck@gmail.com>
2015-12-16 08:53:28 -0800

    finished basic gulpfile. added beginning of express

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,44 @@
 var gulp = require('gulp');
 
-var stylus = require('gulp-stylus');
-var sourcemaps = require('gulp-sourcemaps');
-// var watch = require('gulp-watch');
-
+var concat = require('gulp-concat');
+var del = require('del');
 var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
+var sourcemaps = require('gulp-sourcemaps');
+var stylus = require('gulp-stylus');
+var uglify = require('gulp-uglify');
 
 var paths = {
+  entrypoint: './server/index.js',
   styles: ['./styles/*.styl'],
   js: ['./scripts/**/*.js'],
   vendor: ['./bower_components/*.js'],
   dist: './build'
 };
 
-gulp.task('clean',function(){});
+// Cleanup
+gulp.task('clean',function(){
+  return del(paths.dist)
+});
+
+// Linting
 gulp.task('jshint', function(){
   return gulp.src(paths.js)
     .pipe(jshint('./jshintrc'))
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.js, ['jshint']);
-  gulp.watch(paths.styles, ['build-css']);
+// Builders
+gulp.task('buildApp',function (){
+  return gulp.src(paths.js)
+  .pipe(sourcemaps.init())
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('test', function(){});
-
-gulp.task('build-css',function(){
+gulp.task('buildCss',function(){
   return gulp.src(paths.styles)
   .pipe(sourcemaps.init())
     .pipe(stylus())
@@ -35,12 +46,32 @@ gulp.task('build-css',function(){
   .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('buildVendor', function(){});
-gulp.task('buildApp',function (){});
-
-gulp.task('watch', function() {
-  return gulp.src('')
-
+gulp.task('buildVendor', function(){
+  return gulp.src(paths.vendor)
+  .pipe(sourcemaps.init())
+    .pipe(concat('vendor.js'))
+    .pipe(uglify())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('start', function () {
+  nodemon({
+    script: paths.entrypoint,
+    ext: 'js html',
+    env: { 'NODE_ENV': 'development' }
+  })
+})
+
+gulp.task('watch', function() {
+  gulp.watch(paths.js, ['jshint']);
+  gulp.watch(paths.styles, ['buildCss']);
+  gulp.watch(paths.vendor, ['buildVendor']);
+  gulp.watch(paths.js, ['buildApp']);
+});
+
+
+// Call these
+gulp.task('build', ['buildCss','buildVendor','buildApp'])
+gulp.task('dev',['clean','build','watch','start'])
+gulp.task('default', ['clean','build']);
