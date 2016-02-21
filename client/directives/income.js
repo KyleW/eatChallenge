7eda283d23928981eed99e9aa5468a5b028ec472
KyleW <Kyle.Warneck@gmail.com>
2016-02-20 16:54:09 -0800

    fixed annotations on child income page

--- client/directives/income.js
+++ client/directives/income.js
@@ -27,38 +27,30 @@
                 vaule:'work',
                 label: 'Earnings from work',
                 frequency: ['weekly', 'biweekly', '2x month', 'monthly'],
-                annotation: {
-                    child: 'A child has a job where they earn salary or wages ',
-                    otherMember: '',
-                }
+                annotation: 'A child has a job where they earn salary or wages ',
+                
             },
             socialSecurity: {
                 vaule:'socialSecurity',
                 label: 'Social Security Disability Payments or Survivor’s Benefits ',
                 frequency: ['monthly'],
-                annotation: {
-                    child: 'A child is blind or disabled and receives Social Security benefits.',
+                annotation: 'A child is blind or disabled and receives Social Security benefits.',
                     // ' A parent is disabled, retired, or deceased, and their child receives social security benefits',
-                    otherMember: '',
-                }
+                
             },
             otherPerson: {
                 vaule:'otherPerson',
                 label: 'Income from persons outside the household',
                 frequency: ['weekly', 'biweekly', '2x month', 'monthly'],
-                annotation: {
-                    child: 'A friend or extended family member regularly gives a child spending money',
-                    otherMember: '',
-                }
+                annotation: 'A friend or extended family member regularly gives a child spending money',
+                
             },
             otherSource: {
                 vaule:'otherSource',
                 label: 'Income from any other source',
                 frequency: ['weekly', 'biweekly', '2x month', 'monthly'],
-                annotation: {
-                    child: 'A child receives income from a private pension fund, annuity, or trust',
-                    otherMember: '',
-                }
+                annotation: 'A child receives income from a private pension fund, annuity, or trust',
+                
             }
         };
 
