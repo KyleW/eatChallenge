ce2cbe7d20083f7f58bbe8f151db3004280f83e2
KyleW <Kyle.Warneck@gmail.com>
2016-02-24 22:01:29 -0800

    refactored skip logic

--- client/services/sections.js
+++ client/services/sections.js
@@ -62,6 +62,7 @@
         var indexedSections = _.indexBy(sections, 'state');
 
         var service = {
+            meansTest: true,
             sections: sections,
             indexedSections: indexedSections,
             navigateToNext: navigateToNext,
@@ -73,34 +74,12 @@
         //////////////////////
 
         function updateRequiredSections(household) {
-            var currentChild, studentCount;
-            var assistanceProgramHousehold = false;
-            var specialStatusCount = 0;
-            var skipMeansTest = false;
+            service.meansTest = true;
+            var assistanceProgramHousehold = isAssistanceHousehold(household);
+            var categoricallyQualified = allStudentsCategoricallyQualify(household);
 
-            for (var i = 0 ; i < household.children.length; i++) {
-                currentChild = household.children[i];
-
-                if (currentChild.assistanceProgram && currentChild.assistanceProgram.participant) {
-                    assistanceProgramHousehold = true;
-                    break;
-                }
-
-                if (currentChild.specialStatus && currentChild.enrolled && (currentChild.specialStatus.fosterChild ||
-                    currentChild.specialStatus.homelessMigrantRunaway ||
-                    currentChild.specialStatus.headStartParticipant)
-                ) {
-                    specialStatusCount++;
-                }
-            }
-
-            studentCount = _.where(household.children, {enrolled: true}).length;
-
-            skipMeansTest = assistanceProgramHousehold ||
-                (studentCount > 0 &&
-                 studentCount === specialStatusCount);
-
-            if (skipMeansTest) {
+            if (assistanceProgramHousehold || categoricallyQualified) {
+                service.meansTest = false;
                 indexedSections['childIncome'].required = false;
                 indexedSections['household'].required = false;
                 indexedSections['householdIncome'].required = false;
@@ -110,6 +89,43 @@
             sections.forEach(function(section) {
                 section.required = true;
             });
+        }
+
+        function isAssistanceHousehold(household) {
+            var currentChild;
+            for (var i = 0 ; i < household.children.length; i++) {
+                currentChild = household.children[i];
+                if (currentChild.assistanceProgram && currentChild.assistanceProgram.participant) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function allStudentsCategoricallyQualify(household) {
+            var currentChild, childQualifies;
+
+            var enrolledChildren = _.filter(household.children, function(c) {
+                return c.enrolled === true;
+            });
+
+            // must have kids to qualify
+            if (enrolledChildren.length === 0) {
+                return false;
+            }
+
+            //all children are categorically qualified
+            for (var i = 0 ; i < enrolledChildren.length; i++) {
+                currentChild = household.children[i];
+                if (!currentChild.specialStatus.fosterChild &&
+                    !currentChild.specialStatus.homelessMigrantRunaway &&
+                    !currentChild.specialStatus.headStartParticipant
+                ) {
+                    return false;
+                }
+            }
+
+            return true;
 
         }
 
@@ -132,7 +148,7 @@
         function navigateToNext() {
             var nextSection;
             var currentState = $rootScope.currentState || $state.$current.self.name;
-            var previousState =$rootScope.previousState || 'start';
+            var previousState = $rootScope.previousState || 'start';
 
             // If on the summary page go to the next form page
             if (currentState === 'soFar') {
@@ -143,7 +159,7 @@
                     // Come up with something better? stash in url?
                     nextSection = 'start';
                 }
-                
+
             } else if (indexedSections[currentState].showSummaryAfter) {
                 // if on a form page, go to summary page
                 nextSection = 'soFar';
